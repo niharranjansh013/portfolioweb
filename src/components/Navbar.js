7b030bb569ec8163eccf6d371118b4869068fedc
Nihar Ranjan Sahoo <niharranjansh013@gmail.com>
2024-02-01 20:09:45 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,10 +18,10 @@ export const Navbar = () => {
   return (
       <div className='p-[20px]  flex justify-between '>
                     {
-                        header.map((obj) => {
+                        header.map((obj,ind) => {
                             return (
                                 <div className='text-[25px]'>
-                                    <h1>{obj.port}<span className='text-cyan-600 '>{obj.folio}</span></h1>
+                                    <h1 key={`h1_${ind}`}>{obj.port}<span className='text-cyan-600 '>{obj.folio}</span></h1>
                                 </div>
                             )
                         })
